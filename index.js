fba31184b06add22d6f8893c04034d88a5e0ecc9
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-05 22:40:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+const typeDefs = gql`
+	type Query {
+		sayHi: String!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		sayHi: () => 'Hello World',
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+mongoose.connect(process.env.DATABASE_URI);
+
+server.listen({ port: 5000 }).then(res => {
+	console.log(`Server running at ${res.url}`);
+});
