e24c9b52398c30a5365d81320b1d8b5cccb13520
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-11 00:37:23 -0300

    add posts query

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const FETCH_POSTS_QUERY = gql`
+	query {
+		getPosts {
+			id
+			body
+			createdAt
+			username
+			likes {
+				username
+			}
+			comments {
+				id
+				username
+				createdAt
+				body
+			}
+		}
+	}
+`;
 
 const Home = () => {
+	const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+	if (data) {
+		console.log(data);
+	}
+
 	return (
 		<div>
 			<h1>Home</h1>
