7e9dfd221aeae2c7f803b3053c68960915f04f1d
Tomas <87679143+devtsp@users.noreply.github.com>
2022-04-10 23:30:44 -0300

    add semantic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+// import './App.css';
 
-import MenuBar from './components/MenuBar';
+import MenuBar from './components/MenuBar.js';
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
 import Register from './pages/Register.js';
@@ -12,8 +11,8 @@ import Register from './pages/Register.js';
 function App() {
 	return (
 		<Router>
+			<MenuBar />
 			<Routes>
-				<MenuBar />
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
