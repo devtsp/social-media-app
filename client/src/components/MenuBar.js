b40465a6448f56b59df24f9675834aa66c2e3871
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-11 16:06:14 -0300

    add navbar actual user state

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,14 +1,24 @@
-import { useState } from 'react';
+import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+	const { user, logout } = React.useContext(AuthContext);
 	const pathname = window.location.pathname;
 	const path = pathname === '/' ? 'home' : pathname.substring(1);
-	const [activeItem, setActiveItem] = useState(path);
+	const [activeItem, setActiveItem] = React.useState(path);
 	const handleItemClick = (e, { name }) => setActiveItem(name);
 
-	return (
+	const menuBar = user ? (
+		<Menu pointing secondary size="massive" color="teal">
+			<Menu.Item name={user.username} active as={Link} to="/" />
+			<Menu.Menu position="right">
+				<Menu.Item name="logout" onClick={logout} />
+			</Menu.Menu>
+		</Menu>
+	) : (
 		<Menu pointing secondary size="massive" color="teal">
 			<Menu.Item
 				name="home"
@@ -35,6 +45,8 @@ function MenuBar() {
 			</Menu.Menu>
 		</Menu>
 	);
+
+	return menuBar;
 }
 
 export default MenuBar;
