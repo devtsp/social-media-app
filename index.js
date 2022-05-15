2c0bceef83c4b0573fb74bb6d756437afa91e498
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-15 19:39:29 -0300

    port configuration to heroku deploy

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
@@ -15,8 +17,9 @@ mongoose
 	.connect(process.env.DATABASE_URI, { useNewUrlParser: true })
 	.then(res => {
 		console.log('>> MongoDB connected');
-		return server.listen({ port: 5000 });
+		return server.listen({ port: PORT });
 	})
 	.then(res => {
 		console.log(`>> Server running at ${res.url}`);
-	});
+	})
+	.catch(error => console.error(error));
