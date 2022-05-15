c43be2bff74160b432b143d9ba4945b45a37c23c
Tomas <87679143+devtsp@users.noreply.github.com>
2022-05-15 20:02:33 -0300

    moved client code to dedicated repo

--- client/src/utils/ButtonPopup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Popup } from 'semantic-ui-react';
-
-const ButtonPopup = ({ content, children }) => {
-	return <Popup inverted content={content} trigger={children} />;
-};
-
-export default ButtonPopup;
